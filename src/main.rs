1deea893699caf1177bebe3ab24253c40d368b83
Dandandan <danielheres@gmail.com>
2020-03-01 10:18:28 +0100

    Add nom_locate to add location info to parse results

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,105 @@
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+
 extern crate nom;
+extern crate nom_locate;
+
+use nom_locate::{position, LocatedSpan};
+
+type Span<'a> = LocatedSpan<&'a str>;
+
 use nom::{
     bytes::complete::{tag, take_while1},
     IResult,
 };
 
-fn decl_parser(i: &str) -> IResult<&str, &str> {
+fn decl_parser(i: Span) -> IResult<Span, Span> {
     take_while1(|c: char| c.is_alphabetic())(i)
 }
 
-fn lit_parser(i: &str) -> IResult<&str, &str> {
-    take_while1(|c: char| c.is_digit(10))(i)
+fn lit_parser(i: Span) -> IResult<Span, Span> {
+    take_while1(|c: char| c.is_ascii_digit())(i)
 }
 
-fn parser(i: &str) -> IResult<&str, Expr> {
-    let (i, name) = decl_parser(i)?;
-    let (i, _d) = tag("=")(i)?;
-    let (i, x) = lit_parser(i)?;
+fn parser(i: Span) -> IResult<Span, AnnotatedExpr> {
+    let (_, pos) = position(i)?;
 
+    let (i, name) = decl_parser(i)?;
+
+    let (i, _d) = tag("=")(i)?;
+    let (_, pos2) = position(i)?;
+
+    let (i, expr) = lit_parser(i)?;
+    let parsed_integer = expr.fragment().parse().unwrap();
     Ok((
         i,
-        Expr::Decl(
-            name.to_string(),
-            Box::new(Expr::Literal(Literal::Integer(0))),
-        ),
+        AnnotatedExpr {
+            span: pos,
+            expr: Expr::Equation(Equation {
+                name: &name.fragment(),
+                expr: Box::new(AnnotatedExpr {
+                    span: pos2,
+                    expr: Expr::Literal(Literal::Long(parsed_integer)),
+                }),
+            }),
+        },
     ))
 }
+#[derive(Debug, Eq, PartialEq)]
+struct AnnotatedExpr<'a> {
+    expr: Expr<'a>,
+    span: Span<'a>,
+}
+#[derive(Debug, Eq, PartialEq)]
+enum Expr<'a> {
+    /// Binds an expression to a name
+    Equation(Equation<'a>),
 
-#[derive(Debug)]
-enum Expr {
-    Decl(String, Box<Expr>),
+    /// Literal values
     Literal(Literal),
 }
-#[derive(Debug)]
+
+#[derive(Debug, Eq, PartialEq)]
+struct Equation<'a> {
+    name: &'a str,
+    expr: Box<AnnotatedExpr<'a>>,
+}
+
+#[derive(Debug, Eq, PartialEq)]
 enum Literal {
-    Integer(i32),
+    Long(i64),
 }
 fn main() -> io::Result<()> {
-    let mut file = File::open("example.d")?;
+    let mut file = File::open("example.sql")?;
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let f = parser(&contents);
+    let f = parser(Span::new(&contents));
 
-    println!("Hello, {:?}!", f);
+    println!("Result: {:?}!", f);
 
     Ok(())
+}
+
+#[test]
+fn test_parser() {
+    let res = parser(Span::new("x=1"));
+    assert!(res.is_ok());
+    let res = res.unwrap();
+    assert_eq!(res.1.span.location_offset(), 0);
+    assert_eq!(res.1.span.get_utf8_column(), 1);
+
+    match res.1.expr {
+        Expr::Equation(eq) => {
+            assert_eq!(eq.name, "x");
+            assert_eq!(eq.expr.span.location_offset(), 2);
+            assert_eq!(eq.expr.span.get_utf8_column(), 3);
+
+        }
+        _ => panic!("Did not expect something else than Equation"),
+    }
+
+    //assert_eq!(x, "abc");
 }
